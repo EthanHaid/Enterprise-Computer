f34e2d6326ee33d74ab870a84af3106f2e271077
ehaid <ehaid@sfu.ca>
2020-12-17 19:11:54 -0800

    update readme, add error handling for missing api key

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 Raspberry Pi software to control the speech recognition and lighting aboard the model USS Enterprise NCC-1701-A
 
 Powered by the [SpeechRecognition](https://pypi.org/project/SpeechRecognition/) library
+
+## Raspberry Pi Installation
+Within the repository folder, run the following commands:
+`
+sudo apt-get install libportaudio0 libportaudio2 libportaudiocpp0 portaudio19-dev flac
+pip3 install -r pip-requirements.txt
+`
--- microphone_recognition.py
+++ microphone_recognition.py
@@ -37,6 +37,8 @@ except sr.UnknownValueError:
     print("Google Cloud Speech could not understand audio")
 except sr.RequestError as e:
     print("Could not request results from Google Cloud Speech service; {0}".format(e))
+except AssertionError as e:
+    print("You didn't setup your Google Cloud Speech json credentials; {0}".format(e))
 
 # recognize speech using Wit.ai
 WIT_AI_KEY = "INSERT WIT.AI API KEY HERE"  # Wit.ai keys are 32-character uppercase alphanumeric strings
